2cb66fa8c0949deeaf5974469d8c71ee0b8a0af2
rennunome <r_nunome@emyus.jp>
2021-05-14 16:21:28 +0900

    Stashed files

--- app/Http/Controllers/Top/topController.php
+++ app/Http/Controllers/Top/topController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Top;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class topController extends Controller
 {
         public function showTop()
     {
+       $user_id = Auth::id();
         return view('top.top');
     }
 }
